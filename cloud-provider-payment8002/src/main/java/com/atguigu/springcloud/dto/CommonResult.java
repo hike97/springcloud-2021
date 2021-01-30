76b56be7c0414ba3cbb793fe5928d6baac3797e0
hike97 <seaatom@qq.com>
2021-01-31 00:14:31 +0800

    --springcloud 1.project init --close:

---
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/dto/CommonResult.java
@@ -0,0 +1,24 @@
+package com.atguigu.springcloud.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author hike97 2month
+ * @create 2021-01-25 0:51
+ * @desc 通用返回体
+ **/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommonResult<T> {
+	private Integer code;
+	private String  message;
+	private T       data;
+
+	public CommonResult (Integer code, String message) {
+		this.code = code;
+		this.message = message;
+	}
+}
