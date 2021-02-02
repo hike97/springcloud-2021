ea7851332a0ff131bcb27506e83a1b0b056587b0
hike91 <seaatom@qq.com>
2021-02-02 15:08:10 +0800

    --ribbon

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -21,57 +21,62 @@ import java.util.List;
  **/
 @RestController
 @Slf4j
-@RequestMapping ("payment")
+@RequestMapping("/payment")
 public class PaymentController {
-	@Autowired
-	PaymentService paymentService;
+    @Autowired
+    PaymentService paymentService;
 
-	@Value ("${server.port}")
-	private String serverPort;
+    @Value("${server.port}")
+    private String serverPort;
 
-	@Autowired
-	DiscoveryClient discoveryClient;
+    @Autowired
+    DiscoveryClient discoveryClient;
 
-	/**
-	 * 插入订单操作
-	 *
-	 * @param payment
-	 * @return
-	 */
-	@PostMapping ("/create")
-	public CommonResult create (@RequestBody Payment payment) {
-		int result = paymentService.create (payment);
-		if (result > 0) {
-			return new CommonResult (200, serverPort + ": insert success");
-		} else {
-			return new CommonResult (444, "insert fail");
-		}
-	}
+    /**
+     * 插入订单操作
+     *
+     * @param payment
+     * @return
+     */
+    @PostMapping("/create")
+    public CommonResult create(@RequestBody Payment payment) {
+        int result = paymentService.create(payment);
+        if (result > 0) {
+            return new CommonResult(200, serverPort + ": insert success");
+        } else {
+            return new CommonResult(444, "insert fail");
+        }
+    }
 
-	@GetMapping ("/get/{id}")
-	public CommonResult<Payment> getPaymentById (@PathVariable ("id") Long id) {
-		Payment payment = paymentService.getPaymentById (id);
-		log.info ("test dev tools ~   dasdasdasdas");
-		if (payment != null) {
-			return new CommonResult (200, serverPort + ": select success", payment);
-		} else {
-			return new CommonResult (444, "select fail");
-		}
-	}
+    @GetMapping("/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("test dev tools ~   dasdasdasdas");
+        if (payment != null) {
+            return new CommonResult(200, serverPort + ": select success", payment);
+        } else {
+            return new CommonResult(444, "select fail");
+        }
+    }
 
 
-	@GetMapping (value ="/discovery")
-	public Object discovery () {
-		List<String> services = discoveryClient.getServices ();
-		for (String element : services) {
-			log.info ("***** element:" + element);
-		}
-		List<ServiceInstance> instances = discoveryClient.getInstances ("CLOUD-PAYMENT-SERVICE");
-		for (ServiceInstance instance : instances) {
-			log.info (instance.getServiceId () + "\t" + instance.getHost () + "\t" + instance.getPort () + "\t" + instance.getUri ());
-		}
-		return this.discoveryClient;
-	}
+    @GetMapping(value = "/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            log.info("***** element:" + element);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
+        }
+        return this.discoveryClient;
+    }
+
+    @GetMapping(value = "/lb")
+    public String getPaymentLB() {
+        return serverPort;
+    }
 
 
 }
