e7c7681b7153713f9bf84c297a3ae29a0baf2391
hike91 <seaatom@qq.com>
2021-02-22 17:58:01 +0800

    --nacos

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/atguigu/sringcloud/controller/PaymentController.java
@@ -0,0 +1,24 @@
+package com.atguigu.sringcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName PaymentController
+ * @Description TODO
+ * @Author hike97
+ * @Date 2021/2/22 10:57
+ * @Version 1.0
+ **/
+@RestController
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id) {
+        return "nacos registry, serverPort: " + serverPort + "\t id" + id;
+    }
+}
