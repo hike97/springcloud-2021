be167bcc3ae5c464be41d369ab087b4529c3bdf5
hike91 <seaatom@qq.com>
2021-03-23 18:01:36 +0800

    --seata 1.seata --close:

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/RateLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/RateLimitController.java
@@ -29,7 +29,7 @@ public class RateLimitController {
     }
 
     @GetMapping("/rateLimit/byUrl")
-    @SentinelResource(value = "byUrl")
+    @SentinelResource(value = " ")
     public CommonResult byUrl() {
         return new CommonResult(200, "按url限流测试OK", new Payment(2020L, "serial002"));
     }
